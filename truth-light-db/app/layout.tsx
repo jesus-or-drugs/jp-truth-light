f1c59cceb6c493edb0013c87752b4b8452f1ba01
jesus-or-drugs <masa.loves.it@gmail.com>
2025-12-12 17:46:03 +0900

    新しいテンプレート

--- truth-light-db/app/layout.tsx
+++ truth-light-db/app/layout.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem", maxWidth: "800px", margin: "0 auto" }}>
-      <h1>ファクトシート</h1>
+      <h1>ファクトシート(layout.tsx)</h1>
       <p>
         日本の法規制のもとにある物質、あるいは精神に作用する物質について、
         中立的な情報とハームリダクションの観点からまとめていくプロジェクトです。
