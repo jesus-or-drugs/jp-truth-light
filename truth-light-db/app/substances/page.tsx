0f275809fe4a5ccc53eafa4874db9f3895eb312a
jesus-or-drugs <masa.loves.it@gmail.com>
2025-12-13 03:47:19 +0900

    シンプルにして

--- truth-light-db/app/substances/page.tsx
+++ truth-light-db/app/substances/page.tsx
@@ -11,16 +11,12 @@ type Substance = {
   psychoactive_class: string;
 };
 
-export default async function GetSubstance() {  };
-
-const ViewSubstanceSearchPage =  (substance: Substance) => {
-  return (
+return (
         <main style={{ padding: "2rem", maxWidth: "800px", margin: "0 auto" }}>
         <h1>Fact Sheet of Substances Regulated in Japan</h1>
         <p>
             日本で規制されている、あるいは精神作用を持つ物質の
             法的ステータスと基本的なプロパティをまとめたデータベースです。
         </p>
-        </main>
-    );
-};
+        </main>    
+);
